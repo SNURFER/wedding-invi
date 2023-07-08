077f6746e31cef56816a198f0dcdf05e8422a23a
SNURFER <shido322@snu.ac.kr>
2023-07-08 10:15:19 +0900

    [chore] layout

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,4 +1,4 @@
-<div class="text-center py-5 text-lg">
+<div class="text-center my-20 text-2xl">
     <p>INVITATION</p>
     <p>저희 결혼합니다. </p>
 </div>
--- src/components/MainImage.svelte
+++ src/components/MainImage.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <img src={imgURL} alt="intro" width="678" height="1024" class="w-full pb-5" />
-<div class="text-center justify-center modal-middle flex py-10 text-xl">
+<div class="text-center justify-center modal-middle flex py-5 text-xl">
 	<span class="px-3">이시도</span>
 	<button on:click={() => (alert("since 2017/02/25, 하트수를 count 하자. 진입했을 때마다 딱 한번 밖에 못하게"))}>
-        <span class="px-3 text-4xl text-primary">🖤</span>
+        <span class="px-3 text-xl text-primary">🖤</span>
     </button>
 	<span class="px-3">장소영</span>
 </div>
-<div class="text-center py-10 text-lg">
+<div class="text-center py-5 text-lg">
 	<p>2023년 10월 21일, 오전 11시</p>
 	<p>서울대학교 교수회관</p>
 	<p class="text-xs text-gray-500">(호암 교수회관 아닙니다.)</p>
