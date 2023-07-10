5417d79c307f750ace9a6a7dd060906cfcb70040
SNURFER <shido322@snu.ac.kr>
2023-07-11 00:47:24 +0900

    [test] image test - big size JPEG * 10

--- src/components/Gallery.svelte
+++ src/components/Gallery.svelte
@@ -3,14 +3,14 @@
 
 	let images: string[] = [
 		'image/1.jpeg',
-		'image/2.jpeg'
-		// 'image/3.jpeg',
-		// 'image/4.jpeg',
-		// 'image/5.jpeg',
-		// 'image/7.jpeg',
-		// 'image/8.jpeg',
-		// 'image/9.jpeg',
-		// 'image/10.jpeg',
+		'image/2.jpeg',
+		'image/3.jpeg',
+		'image/4.jpeg',
+		'image/5.jpeg',
+		'image/7.jpeg',
+		'image/8.jpeg',
+		'image/9.jpeg',
+		'image/10.jpeg'
 		// 'image/11.jpeg',
 		// 'image/12.jpeg',
 		// 'image/13.jpeg',
