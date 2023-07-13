0a10f005180b14ab3bdcd86ef0d359e55d1268d9
SNURFER <shido322@snu.ac.kr>
2023-07-13 20:11:29 +0900

    [feat] add analytics

---
+++ src/lib/analytics.svelte
@@ -0,0 +1,13 @@
+<!-- Google tag (gtag.js) -->
+<svelte:head>
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-KG3L8HZ4P1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-KG3L8HZ4P1');
+	</script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script>
 	import '../app.css';
+	import Analytics from '../lib/analytics.svelte';
 </script>
 
+<Analytics />
 <div class="font-mywife max-w-3xl m-auto">
 	<slot />
 </div>
